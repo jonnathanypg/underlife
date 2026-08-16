816d4a1beff52879baed57b681269f54438f2382
jonnathanypg <jonnathan.ypg@gmail.com>
2026-08-16 08:40:57 -0500

    chore(pm2): optimizar ecosystem.config.js para ejecucion estable en produccion con PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'underlife-prod',
       script: './node_modules/next/dist/bin/next',
       args: 'start -p ' + (process.env.PORT || 3000),
+      node_args: '--no-node-snapshot',
       cwd: __dirname,
       instances: 1,
       exec_mode: 'fork',
